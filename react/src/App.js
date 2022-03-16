70bd7fa4200c0d07de8764a4554c68b9d0daa9f4
M0lik <guillaume.verneret@gmail.com>
2022-03-16 17:45:46 +0100

    add react exercice

---
+++ react/src/App.js
@@ -0,0 +1,37 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+   axios({
+      method: "get",
+      url: 'http://localhost:3001/',
+    }).then(response => {
+      console.log(response.data);
+      setData(response.data)
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {data.map(e => <p>{e._id}</p>)}
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
